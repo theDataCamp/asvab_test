28940c1408fe90997576abafe5510a57a15785f4
aleman87 <josue.aleman87@gmail.com>
2017-08-02 16:44:05 -0700

    updated to add rates questions
    - updated generateAnswersDouble to include a rounding option
    -created a generateAnswersMoney to display answers involving money

--- math_part.cpp
+++ math_part.cpp
@@ -7,11 +7,12 @@
 //
 
 #include <iostream>
-#include "shapes.h"
 #include <time.h>
 #include <iomanip>
 #include <vector>
 #include <math.h>
+#include <string>
+#include <limits>
 
 #define PI 3
 using namespace std;
@@ -20,10 +21,14 @@ using namespace std;
 void dividingQuestion();
 void areaQuestion();
 void exponentQuestion();
-void foilQuestion();
+void percentageQuestion();
+void rateQuestion();
+void foilQuestion(vector<int> &);
 
-char generateAnswerDouble(double, bool);
-char generateAnswerInt(int,bool);
+char generateAnswerDouble(double, bool,string,bool);
+char generateAnswerInt(int,bool, string);
+char generateAnswerMoney(double,bool,string,bool);
+
 char validateQuestion();
 void checkAnswers(char, char);
 
@@ -35,16 +40,47 @@ vector<int> foilEval(int,int,int,int);//a,b,c,d passed ex.(a+b)(c+d)
 int main()
 {
     srand(time(NULL));
+    //char continueKey;
+    cout<<"ASVAB PREP  v.1.0"<<endl;
+    cout<<"by: Josue Aleman"<<endl<<endl<<endl;
+    cout<<"Press [ENTER] to continue...";
+    //cin>>continueKey;
+    cin.ignore(std::numeric_limits<streamsize>::max(),'\n');
+    bool yesno=true;
+    char response;
+    while(yesno){
     
-    dividingQuestion();
-    cout<<endl;
-    areaQuestion();
-    cout<<endl;
-    exponentQuestion();
-    cout<<endl;
+   // cout << string(50, '\n');
+        cout<<endl<<endl;
+        rateQuestion();
+        cout<<endl;
+//    dividingQuestion();
+//    cout<<endl;
+//    areaQuestion();
+//    cout<<endl;
+//    exponentQuestion();
+//    cout<<endl;
+//    percentageQuestion();
+//    cout<<endl;
+//    
+        cout<<"Continue? (y/n):";
+        cin>>response;
+        while(!cin || !(response=='y'||response=='Y'||response=='n'||response=='N'))
+        {
+            cout<<"Error! Please select (y/n): ";
+            cin.clear();
+            cin.ignore();
+            cin>>response;
+        }
+        if(response=='n'||response=='N'){yesno=false;}
+        else if (response=='y'||response=='Y'){yesno=true;}
+        
+
+        
+    }
         return 0;
 }
-char generateAnswerDouble(double answer, bool far)
+char generateAnswerDouble(double answer, bool far, string addon, bool round_num)
 {
     srand(time(NULL));
     int answerIndex=randomInt(0, 3);//gives index between 0-3
@@ -68,14 +104,25 @@ char generateAnswerDouble(double answer, bool far)
             answers.push_back(values[i]);
         }
     }
+    if (round_num)
+    {
+        
+        cout<<"a)"<<round(answers[0])<<addon<<endl;
+        cout<<"b)"<<round(answers[1])<<addon<<endl;
+        cout<<"c)"<<round(answers[2])<<addon<<endl;
+        cout<<"d)"<<round(answers[3])<<addon<<endl;
+    
+    }
+    else{
     cout<<fixed<<setprecision(2);
-    cout<<"a)"<<answers[0]<<endl;
-    cout<<"b)"<<answers[1]<<endl;
-    cout<<"c)"<<answers[2]<<endl;
-    cout<<"d)"<<answers[3]<<endl;
+    cout<<"a)"<<answers[0]<<addon<<endl;
+    cout<<"b)"<<answers[1]<<addon<<endl;
+    cout<<"c)"<<answers[2]<<addon<<endl;
+    cout<<"d)"<<answers[3]<<addon<<endl;
+    }
     return rightanswer;
 }
-char generateAnswerInt(int answer, bool far)
+char generateAnswerInt(int answer, bool far, string addon)
 {
     srand(time(NULL));
     int answerIndex=randomInt(0, 3);//gives index between 0-3
@@ -100,12 +147,57 @@ char generateAnswerInt(int answer, bool far)
         }
     }
     cout<<fixed<<setprecision(2);
-    cout<<"a)"<<answers[0]<<endl;
-    cout<<"b)"<<answers[1]<<endl;
-    cout<<"c)"<<answers[2]<<endl;
-    cout<<"d)"<<answers[3]<<endl;
+    cout<<"a)"<<answers[0]<<addon<<endl;
+    cout<<"b)"<<answers[1]<<addon<<endl;
+    cout<<"c)"<<answers[2]<<addon<<endl;
+    cout<<"d)"<<answers[3]<<addon<<endl;
     return rightanswer;
 }
+
+char generateAnswerMoney(double answer, bool far, string addon, bool round_num)
+{
+    srand(time(NULL));
+    int answerIndex=randomInt(0, 3);//gives index between 0-3
+    char rightanswer=96;
+    double val1,val2,val3,val4;
+    if(far){val1=randomInt(1, 20);val2=randomInt(1, 20);val3=randomInt(21, 70);val4=randomInt(21, 50);}
+    else{val1=randomInt(1, 3);val2=randomInt(1, 3);val3=randomInt(5, 15);val4=randomInt(2, 5);}
+    val1=val1*answer+1;val2=answer/val2+2;val3=answer+val3+1;val4=answer*val4/val1+val3+1;
+    vector<double> values={val1,val2,val3,val4};
+    vector<double> answers={};
+    for(int i=0; i<4;i++)
+    {
+        if(i==answerIndex)
+        {
+            answers.push_back(answer);
+            rightanswer=rightanswer+i+1;
+            
+        }
+        else
+        {
+            answers.push_back(values[i]);
+        }
+    }
+    if (round_num)
+    {
+        cout<<fixed<<setprecision(2);
+        cout<<"a) $"<<round(answers[0])<<addon<<endl;
+        cout<<"b) $"<<round(answers[1])<<addon<<endl;
+        cout<<"c) $"<<round(answers[2])<<addon<<endl;
+        cout<<"d) $"<<round(answers[3])<<addon<<endl;
+        
+    }
+    else{
+        cout<<fixed<<setprecision(2);
+        cout<<"a) $"<<answers[0]<<addon<<endl;
+        cout<<"b) $"<<answers[1]<<addon<<endl;
+        cout<<"c) $"<<answers[2]<<addon<<endl;
+        cout<<"d) $"<<answers[3]<<addon<<endl;
+    }
+    return rightanswer;
+}
+
+
 char validateQuestion()
 {
     char selection;
@@ -149,7 +241,7 @@ void dividingQuestion()
             else{num2=randomInt(50, 100);}
             cout<<"What is "<<num1<<"/"<<num2<<"?"<<endl;
             answer=num1/num2;
-            right_answer=generateAnswerDouble(answer, true);
+            right_answer=generateAnswerDouble(answer, true,"",false);
             user_choice=validateQuestion();
             checkAnswers(user_choice, right_answer);
             break;
@@ -163,7 +255,7 @@ void dividingQuestion()
             else{num2=randomInt(1, 9);num2=num2/10;}
             cout<<"What is "<<num1<<"/"<<fixed<<setprecision(2)<<num2<<"?"<<endl;
             answer=num1/num2;
-            right_answer=generateAnswerDouble(answer, false);
+            right_answer=generateAnswerDouble(answer, false,"",false);
             user_choice=validateQuestion();
             checkAnswers(user_choice, right_answer);
             break;
@@ -175,7 +267,7 @@ void dividingQuestion()
             num2=num2/100;
             cout<<"What is "<<fixed<<setprecision(2)<<num1<<"/"<<num2<<"?"<<endl;
             answer=num1/num2;
-            right_answer=generateAnswerDouble(answer, false);
+            right_answer=generateAnswerDouble(answer, false,"",false);
             user_choice=validateQuestion();
             checkAnswers(user_choice, right_answer);
             break;
@@ -199,7 +291,7 @@ void areaQuestion()
             area=length*width;
             cout<<"The area of a RECTANGLE with length "<<fixed<<setprecision(0)<<length;
             cout<<" and width "<<width<<" is:"<<endl;
-            right_answer=generateAnswerInt(int(area), true);
+            right_answer=generateAnswerInt(int(area), true,"");
             user_choice=validateQuestion();
             checkAnswers(user_choice, right_answer);
             break;
@@ -207,7 +299,7 @@ void areaQuestion()
             length=randomInt(5, 85);
             area=length*length;
             cout<<"The area of a SQUARE with length "<<fixed<<setprecision(0)<<length<<" is:"<<endl;
-            right_answer=generateAnswerInt(int(area), true);
+            right_answer=generateAnswerInt(int(area), true,"");
             user_choice=validateQuestion();
             checkAnswers(user_choice, right_answer);
             break;
@@ -217,7 +309,7 @@ void areaQuestion()
             area=(width*height)/2.0;
             cout<<"The area of a TRIANGLE with height "<<fixed<<setprecision(0)<<height;
             cout<<" and width "<<width<<" is:"<<endl;
-            right_answer=generateAnswerDouble(area, true);
+            right_answer=generateAnswerDouble(area, true,"",false);
             user_choice=validateQuestion();
             checkAnswers(user_choice, right_answer);
             break;
@@ -225,7 +317,7 @@ void areaQuestion()
             radius=randomInt(5, 45);
             area=PI*radius*radius;
             cout<<"The area of a CIRCLE with radius "<<fixed<<setprecision(0)<<radius<<" is:"<<endl;
-            right_answer=generateAnswerInt(int(area), true);
+            right_answer=generateAnswerInt(int(area), true,"");
             user_choice=validateQuestion();
             checkAnswers(user_choice, right_answer);
             break;
@@ -252,7 +344,7 @@ void exponentQuestion()
         case 0://simple exponent question (ie 4^2=)
             answer=pow(baseNum1,exponent1);
             cout<<"The value of "<<fixed<<setprecision(0)<<baseNum1<<"^"<<exponent1<<" is:"<<endl;
-            right_answer=generateAnswerInt(answer, false);
+            right_answer=generateAnswerInt(answer, false,"");
             user_choice=validateQuestion();
             checkAnswers(user_choice, right_answer);
             break;
@@ -260,7 +352,7 @@ void exponentQuestion()
             answer=(pow(baseNum1,exponent1)+pow(baseNum2,exponent2));
             cout<<fixed<<setprecision(0);
             cout<<baseNum1<<"^"<<exponent1<<"+"<<baseNum2<<"^"<<exponent2<<"="<<endl;
-            right_answer=generateAnswerInt(answer, false);
+            right_answer=generateAnswerInt(answer, false,"");
             user_choice=validateQuestion();
             checkAnswers(user_choice, right_answer);
             break;
@@ -270,12 +362,132 @@ void exponentQuestion()
             cout<<fixed<<setprecision(0);
             cout<<"Solve for x in: "<<baseNum1<<"^"<<exponent1;
             cout<<"*"<<baseNum1<<"^x ="<<baseNum1<<"^"<<exponent3<<endl;
-            right_answer=generateAnswerInt(answer, false);
+            right_answer=generateAnswerInt(answer, false,"");
             user_choice=validateQuestion();
             checkAnswers(user_choice, right_answer);
             break;
             
     }
+}
+void percentageQuestion()
+{
+    srand(time(NULL));
+    int selection=randomInt(0, 2);
+    //int selection=1;
+    double num1, num2,answer=0.0;
+    char user_choice;
+    char right_answer;
+    
+    switch(selection)
+    {
+        case 0:{//What is _num1_% of _num2_?
+            double percent_num=double(randomInt(1, 98)),
+            num_val=double(randomInt(50, 600));
+            cout<<fixed<<setprecision(0);
+            cout<<"What is "<<percent_num<<"% of "<<num_val<<"?"<<endl;
+            answer=(percent_num/100)*num_val;
+            right_answer=generateAnswerDouble(answer, false,"",false);
+            user_choice=validateQuestion();
+            checkAnswers(user_choice, right_answer);
+            break;}
+            
+        case 1:{//If the total is __ before taxes and ___ after taxes, the tax rate is?
+            double before_tax=double(randomInt(80, 400)),
+                    after_tax=0.0,
+                    tax_rate=double(randomInt(5, 25)); //keeping tax rate bw 5-25%
+            answer=tax_rate;
+            tax_rate/=100.0;
+            after_tax=(before_tax*tax_rate)+before_tax;
+            cout<<fixed<<setprecision(2);
+            cout<<"If the total is $"<<before_tax<<" before taxes and $"<<after_tax<<" after taxes, the tax rate is?"<<endl;
+            right_answer=generateAnswerDouble(answer, false,"%",false);
+            user_choice=validateQuestion();
+            checkAnswers(user_choice, right_answer);
+            break;}
+            
+        }
+
+}
+
+void rateQuestion()
+{
+    srand(time(NULL));
+    double rate, numerator, denominator, answer;
+    char user_choice;
+    char right_answer;
+    
+    int selection=3;
+    //int selection=randomInt(0, 4);
+    
+    switch (selection)
+    {
+        case 0:{//soldier can run ____ miles in ___ time What is the average pace? (mins/mile)
+            numerator=double(randomInt(20, 35));
+            denominator=double(randomInt(3, 6));
+            rate=numerator/denominator;
+            cout<<"A soldier can run "<<denominator<<" miles in "<<numerator<<" minutes."<<endl;
+            cout<<"What is the average pace of the soldier? (mins/mile)"<<endl;
+            answer=rate;
+            right_answer=generateAnswerDouble(answer, false, " mins/mile",false);
+            user_choice=validateQuestion();
+            checkAnswers(user_choice, right_answer);
+            break;
+        }
+        case 1:{//a cook can bake ___ cakes in ___ days How many can he bake in __ days?
+            numerator=double(randomInt(20, 50));
+            denominator=double(randomInt(7, 15));
+            double days=double(randomInt(18, 35));
+            rate=numerator/denominator;
+            cout<<"A cook can bake "<<numerator<<" cakes in "<<denominator<<" days."<<endl;
+            cout<<"How many cakes can they bake in "<<days<<" days?"<<endl;
+            cout<<"(Round answer to nearest whole num)"<<endl;
+            answer=rate*days;
+            right_answer=generateAnswerDouble(answer, false, " cakes",true);
+            user_choice=validateQuestion();
+            checkAnswers(user_choice, right_answer);
+            break;
+        }
+        case 2:{//____ weapons cost ___ How much does ___ rifles cost?
+            numerator=double(randomInt(600, 2500));
+            denominator=double(randomInt(5, 30));
+            double weapons=double(randomInt(33, 120));
+            rate=numerator/denominator;
+            cout<<denominator<<" weapons cost $"<<fixed<<setprecision(2)<<numerator<<"."<<endl;
+            cout<<"How much do "<<fixed<<setprecision(0)<<weapons<<" weapons cost?"<<endl;
+            answer=rate*weapons;
+            right_answer=generateAnswerMoney(answer, false, "", false);
+            user_choice=validateQuestion();
+            checkAnswers(user_choice, right_answer);
+            break;
+        }
+        case 3:{//A vehicle's tank is full and drives ___ miles. It takes ____ to fill the tank. What is the mpg (miles per gallon) of the vehicle?
+            numerator=double(randomInt(100, 350));
+            denominator=double(randomInt(10, 20));
+            rate=numerator/denominator;
+            answer=rate;
+            cout<<"A vehicle's tank is full and drives "<<numerator<<" miles."<<endl;
+            cout<<"It takes "<<denominator<<" gallons  to fill the tank again."<<endl;
+            cout<<"What is the MPG (miles per gallon) of the vehicle?"<<endl;
+            right_answer=generateAnswerDouble(answer, false, " mpg", false);
+            user_choice=validateQuestion();
+            checkAnswers(user_choice, right_answer);
+            break;
+        }
+    }
+    
+}
+
+char generateFoilAnswers(vector<int>& foilResult)
+{char rightanswer=96;
+    int answerIndex=randomInt(0, 3);
+    bool allPostiveNums=true;
+    for (int i=0;i<sizeof(foilResult);i++)
+    {
+        if(foilResult[i]<0){allPostiveNums=false;}
+    }
+    int num1,num2,num3;
+    
+    return rightanswer;
 }
 
 void foilQuestion()
@@ -337,3 +549,4 @@ int randomInt(int start, int end)
     
     return randnum;
 }
+
